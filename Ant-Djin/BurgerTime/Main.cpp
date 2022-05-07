a77826e52de644fbf662b92ffaebba8c1aa1b8d6
Jitse Verstraete <jitse.verstraete@gmail.com>
2022-05-07 13:54:03 +0200

    split engine from project step 1 and 2

---
+++ Ant-Djin/BurgerTime/Main.cpp
@@ -0,0 +1,15 @@
+#include "pch.h"
+
+#if _DEBUG
+#if __has_include(<vld.h>)
+#include <vld.h>
+#endif
+#endif
+
+#include "Minigin.h"
+
+int main(int, char* []) {
+	dae::Minigin engine;
+	engine.Run();
+	return 0;
+}
